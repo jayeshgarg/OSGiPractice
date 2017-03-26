bd92e1f44e25c2128b0f70aacef37ee236bbf178
Jayesh Garg <jayesh.garg@ericsson.com>
2017-03-26 14:07:56 +0530

    Corrections for IT

--- com.gargjayesh.practice.karaf/integrationtest/testcases/src/test/java/com/gargjayesh/practice/karaf/integrationtest/testcases/ITTemperatureRestTest.java
+++ com.gargjayesh.practice.karaf/integrationtest/testcases/src/test/java/com/gargjayesh/practice/karaf/integrationtest/testcases/ITTemperatureRestTest.java
@@ -1,23 +1,22 @@
 package com.gargjayesh.practice.karaf.integrationtest.testcases;
 
-import static org.apache.karaf.tooling.exam.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.junit.Assert.assertNotNull;
 import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.workingDirectory;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 
 import javax.inject.Inject;
 import java.io.File;
 
-import org.apache.karaf.tooling.exam.options.KarafDistributionConfigurationFilePutOption;
-import org.apache.karaf.tooling.exam.options.KarafDistributionConfigurationFileReplacementOption;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.OptionUtils;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.BundleContext;
@@ -34,17 +33,6 @@ public class ITTemperatureRestTest
 
     private static final Logger LOG = LoggerFactory.getLogger(ITTemperatureRestTest.class);
 
-    static
-    {
-        try
-        {
-            Class.forName("org.ops4j.pax.exam.options.extra.FeaturesScannerProvisionOption");
-        } catch (Exception e)
-        {
-            LOG.error("Class def not found");
-        }
-    }
-
     @Inject
     private BundleContext bundleContext;
 
@@ -55,35 +43,26 @@ public class ITTemperatureRestTest
     private TemperatureSensor sensor;
 
     @Configuration
-    public static Option[] configuration()
+    public Option[] config()
     {
-        Option[] testBundles = options(
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.views").artifactId("entities").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.sensor").artifactId("api").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.sensor").artifactId("impl").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.weatherdisplay").artifactId("api").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.weatherdisplay").artifactId("impl").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.displayunit").artifactId("api").version("0.1-SNAPSHOT"),
-                mavenBundle().groupId("com.gargjayesh.practice.karaf.displayunit").artifactId("impl").version("0.1-SNAPSHOT"));
-        Option[] abc = config();
-        Option[] options = OptionUtils.combine(testBundles, abc);
-        return options;
-    }
-
-    private static Option[] config()
-    {
+        MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf").version("3.0.0").type("tar.gz");
+        MavenUrlReference karafStandardRepo = maven().groupId("org.apache.karaf.features").artifactId("standard").classifier("features").type("xml").versionAsInProject();
         return new Option[]{
-                karafDistributionConfiguration().frameworkUrl(maven().groupId("org.apache.karaf").artifactId("apache-karaf").type("zip").versionAsInProject()).karafVersion("2.3.0").name("Apache Karaf"),
-                new KarafDistributionConfigurationFileReplacementOption("etc/org.ops4j.pax.url.mvn.cfg", new File("src/test/resources/org.ops4j.pax.url.mvn.cfg")),
-                new KarafDistributionConfigurationFilePutOption("etc/jmsconfig.cfg", "jmstransportconnector.uri", "tcp://0.0.0.0:61616"),
-                new KarafDistributionConfigurationFilePutOption("etc/jmsconfig.cfg", "connectionfactory.uri", "vm://localhost"),
-                workingDirectory("target/paxrunner/features/")
+                // KarafDistributionOption.debugConfiguration("5005", true),
+                karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target/exam")).useDeployFolder(false), keepRuntimeFolder(),
+                features(karafStandardRepo, "scr"),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.views").artifactId("entities").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.sensor").artifactId("api").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.sensor").artifactId("impl").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.weatherdisplay").artifactId("api").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.weatherdisplay").artifactId("impl").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.displayunit").artifactId("api").version("0.1-SNAPSHOT").start(),
+                mavenBundle().groupId("com.gargjayesh.practice.karaf.displayunit").artifactId("impl").version("0.1-SNAPSHOT").start()
         };
-
     }
 
     @Test
-    public void test() throws Exception
+    public void testRestCode() throws Exception
     {
         LOG.debug("Test case started");
         assertNotNull(sensor.getTemperature());
