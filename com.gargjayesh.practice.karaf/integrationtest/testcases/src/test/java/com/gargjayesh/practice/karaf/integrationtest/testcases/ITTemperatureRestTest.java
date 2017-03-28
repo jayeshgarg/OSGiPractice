884c3599e2a733f79b86010b7b22f9c9c9dff3b6
Jayesh Garg <jayesh.garg@ericsson.com>
2017-03-28 23:27:54 +0530

    Fixed IT case execution code

--- com.gargjayesh.practice.karaf/integrationtest/testcases/src/test/java/com/gargjayesh/practice/karaf/integrationtest/testcases/ITTemperatureRestTest.java
+++ com.gargjayesh.practice.karaf/integrationtest/testcases/src/test/java/com/gargjayesh/practice/karaf/integrationtest/testcases/ITTemperatureRestTest.java
@@ -10,8 +10,17 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDist
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 
 import javax.inject.Inject;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -26,21 +35,28 @@ import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.gargjayesh.practice.karaf.sensor.api.TemperatureSensor;
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class) //Pax container OSGI RT starter configuration
 public class ITTemperatureRestTest
 {
 
+    public static final long MAX_CONTAINER_STARTUP_TIME = 8 * 10 * 1000;
+
     private static final Logger LOG = LoggerFactory.getLogger(ITTemperatureRestTest.class);
 
     @Inject
-    private BundleContext bundleContext;
+    @org.ops4j.pax.exam.util.Filter(timeout = MAX_CONTAINER_STARTUP_TIME)
+    protected TemperatureSensor sensor;
 
     @Inject
-    private ConfigurationAdmin configurationAdmin;
+    @org.ops4j.pax.exam.util.Filter(timeout = MAX_CONTAINER_STARTUP_TIME)
+    protected BundleContext bundleContext;
 
-    //@Inject
-    //private TemperatureSensor sensor;
+    @Inject
+    @org.ops4j.pax.exam.util.Filter(timeout = MAX_CONTAINER_STARTUP_TIME)
+    protected ConfigurationAdmin configurationAdmin;
 
     @Configuration
     public Option[] config()
@@ -48,10 +64,14 @@ public class ITTemperatureRestTest
         MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf").version("3.0.6").type("tar.gz");
         MavenUrlReference karafStandardRepo = maven().groupId("org.apache.karaf.features").artifactId("standard").classifier("features").version("3.0.6").type("xml");
         return new Option[]{
-                //set this to ture if we want to enable remote debugging in karaf
+                //set this to true if we want to enable remote debugging in karaf
                 //debugConfiguration("5005", false),
 
+                //option to clean exam folder after TCs execution
                 keepRuntimeFolder(),
+
+                //option to increase or decrease karaf service lookup timeout time, if yo have more bundles, increase this
+                //new RBCLookupTimeoutOption(TimeUnit.MINUTES.toMillis(10)),
 
                 //option to configure where the test karaf will get extracted
                 karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target/exam")).useDeployFolder(false),
@@ -61,12 +81,13 @@ public class ITTemperatureRestTest
 
                 configureConsole().ignoreLocalConsole(),
 
-                //list of bundles project depends
+                //list of 3rd party project dependencies
                 mavenBundle().groupId("com.eclipsesource.jaxrs").artifactId("jersey-all").version("2.22.2").start(),
-                //mavenBundle().groupId("com.google.code.gson").artifactId("gson").version("2.8.0").start(),
                 mavenBundle().groupId("com.eclipsesource.jaxrs").artifactId("publisher").version("5.0").start(),
-                //mavenBundle().groupId("org.osgi").artifactId("org.osgi.core").version("6.0.0").start(),
                 bundle("wrap:mvn:com.google.code.gson/gson/2.8.0"),
+                bundle("wrap:mvn:org.apache.httpcomponents/httpclient/4.5.3"),
+                bundle("wrap:mvn:org.apache.httpcomponents/httpcore/4.4.6"),
+
                 //list of project bundles under test
                 mavenBundle().groupId("com.gargjayesh.practice.karaf.views").artifactId("entities").version("0.1-SNAPSHOT").start(),
                 mavenBundle().groupId("com.gargjayesh.practice.karaf.sensor").artifactId("api").version("0.1-SNAPSHOT").start(),
@@ -81,9 +102,29 @@ public class ITTemperatureRestTest
     @Test
     public void testRestCode() throws Exception
     {
-        LOG.debug("Test case started");
-        assertNotNull("Testing started");
-        //assertNotNull(sensor.getTemperature());
+        LOG.error("Test case started");
+        TimeUnit.SECONDS.sleep(10);
+
+        String URL = "http://localhost:8181/weather/temperature";
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet getRequest = new HttpGet(URL);
+        getRequest.setHeader("User-Agent", "MySuperUserAgent");
+        HttpResponse response = client.execute(getRequest);
+        String responseEntity = convertResponseEntityToString(response.getEntity());
+        LOG.warn("responseEntity = {}", responseEntity);
+        assertNotNull(responseEntity);
+    }
+
+    private String convertResponseEntityToString(HttpEntity entity) throws IOException{
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(entity.getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
     }
 
 }
